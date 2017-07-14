924f2290d0683a73a68769d33fdadece2af4e0b1
Blessing Pascal <pascal.blessing@gmx.at>
2017-07-14 16:44:57 +0200

    Use typescript parser

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const program = require('commander');
 const kebabcase = require('lodash.kebabcase');
 const os = require('os');
+const ts = require('byots');
 
 
 let fileValue;
 
-let importBlock;
-
-const functionNames = [];
+const dependencyImports = [];
+const definedFunctions = {};
+const barrelExportDeclarations = [];
+const barrelExportAssignments = [];
 
 program
     .version('0.1.0')
@@ -18,95 +20,106 @@ program
     })
     .parse(process.argv)
 
+
+
+
 // read file passed as parameter
-fs.readFile(fileValue, (err, data) => {
-    if (err) {
-        console.log(err);
-    }
+const data = fs.readFileSync(fileValue)
 
-    // get filename without file ending
-    const fileName = fileValue.substring(0, fileValue.lastIndexOf('.'));
+// get filename without file ending
+const fileName = fileValue.substring(0, fileValue.lastIndexOf('.'));
 
-    // create folder with same name as the file (without ending)
-    fs.mkdir(fileName, (err) => {
-        if (err) {
-            console.log(err);
+// create folder with same name as the file (without ending)
+if (!fs.existsSync(fileName)) {
+    fs.mkdirSync(fileName);
+}
+
+
+let content = data.toString();
+
+const sourceFile = ts.createSourceFile(fileValue, content,
+    ts.ScriptTarget.ES5,
+    true,
+    ts.LanguageVariant.Standard
+);
+
+
+
+parseAllChildren(sourceFile);
+
+// generate import block from outsourced functions
+const indexImportBlock = Object.keys(definedFunctions).map(functionName => `import { ${functionName} } from './${kebabcase(functionName)}';`).join(os.EOL);
+
+// create index.ts
+fs.writeFile(
+    fileName + '/index.ts',
+    indexImportBlock + os.EOL +
+    barrelExportDeclarations.join('') +
+    barrelExportAssignments.join('') +
+    os.EOL
+);
+
+
+// write file on fileName/function-name.ts with imports, comment and exported function
+Object.keys(definedFunctions).forEach(functionName => {
+    const outsourcedContent =
+        // TODO: only import those that are needed
+        dependencyImports.join('')
+        // get relative imports one level higher
+        .replace(/'\.{2}\//g, `'../../`)
+        .replace(/'\.\//g, `'../`) +
+        os.EOL +
+        // TODO: only import those that are needed
+        // cross-import own files to have all functions available
+        Object.keys(definedFunctions).filter(func => func !== functionName).map(func => `import { ${func} } from './${kebabcase(func)}';`).join(os.EOL) +
+        os.EOL +
+        definedFunctions[functionName] + os.EOL + os.EOL +
+        `export { ${functionName} };` + os.EOL +
+        `export default { ${functionName} };` + os.EOL;
+
+    fs.writeFile(
+        fileName + '/' + kebabcase(functionName) + '.ts',
+        outsourcedContent,
+        err => {
+            if (err) {
+                console.error(err);
+            }
         }
-
-        // file content
-        let content = data.toString();
-
-        let startPointer = 0;
-        let endPointer = 0;
-        let lastEnd = 0;
-
-        // loop until no more functions are found
-        while (true) {
-            // find start of function statement
-            startPointer = content.indexOf('function', startPointer);
-            // find start of opening curly
-            endPointer = content.indexOf(')', startPointer) // check if round brackets are allowed in parameter list
-            if (endPointer === -1) {
-                break;
-            }
-            endPointer = content.indexOf('{', endPointer)
-
-            // if non left quit
-            if (startPointer === -1 || endPointer === -1) {
-                break;
-            }
-
-            // start with first curly
-            let countBraces = 1;
-            endPointer++;
-
-
-            // count curlies until closing bracket for the function statement is found
-            // FIXME: fails with typescript function return type containing brackets
-            while (countBraces > 0) {
-                if (content[endPointer] === '{') {
-                    countBraces++;
-                } else if (content[endPointer] === '}') {
-                    countBraces--;
-                }
-                endPointer++;
-            }
-
-            // FIXME: might be problematic with nested functions
-            const jsdocRegex = /(\/\*\*[\s\S]*\*\/[\s]*)function/
-            const jsdocMatch = jsdocRegex.exec(content.substring(lastEnd, endPointer))
-            const jsdocComment = jsdocMatch ? jsdocMatch[1] : ''
-
-            const functionCode = content.substring(startPointer, endPointer);
-
-            // on first found function store everything before that to attach it to every file
-            if (!importBlock) {
-                const importBlockEnd = !!jsdocComment ? content.indexOf(jsdocComment) : content.indexOf(functionCode);
-                importBlock = content.substring(0, importBlockEnd)
-                    // get relative imports one level higher
-                    .replace(/'\.{2}\//g, `'../../`)
-                    .replace(/'\.\//g, `'../`);
-
-
-            }
-
-            const functionNameRegex = /function\s+([^(\s]*)\s*\(/;
-            const functionName = functionNameRegex.exec(functionCode)[1];
-
-            // add to list of outsourced functions
-            functionNames.push(functionName);
-
-            // write file on fileName/function-name.ts with imports, comment and exported function
-            fs.writeFile(fileName + '/' + kebabcase(functionName) + '.ts', importBlock + jsdocComment + 'export ' + functionCode);
-
-            // continue from after ending curly
-            startPointer = lastEnd = endPointer;
-        }
-
-        // generate import block from outsourced functions
-        const indexImportBlock = functionNames.map(functionName => `import { ${functionName} } from './${kebabcase(functionName)}';`).join(os.EOL);
-
-        // create index.ts
-        fs.writeFile(fileName + '/index.ts', indexImportBlock + os.EOL + content.substring(lastEnd) + os.EOL)
-    })
+    );
 })
+
+
+function parseAllChildren(node, depth = 0) {
+    if (depth === 2) {
+        const nodeContent = content.substring(node.pos, node.end);
+
+        switch (node.kind) {
+            case ts.SyntaxKind.FunctionDeclaration:
+                const functionName = node.name.text;
+
+                console.log(ts.formatSyntaxKind(node.kind), functionName, node.pos, node.end);
+
+                definedFunctions[functionName] = nodeContent;
+
+                break;
+            case ts.SyntaxKind.ImportDeclaration:
+                dependencyImports.push(nodeContent);
+                break;
+            case ts.SyntaxKind.ExportAssignment:
+                barrelExportAssignments.push(nodeContent);
+                break;
+            case ts.SyntaxKind.ExportDeclaration:
+                barrelExportDeclarations.push(nodeContent);
+                break;
+            default:
+                console.warn('Unexpected element: ', ts.formatSyntaxKind(node.kind), ts.content.substring(node.pos, node.end), '. Please check the according code and copy it yourself.');
+                break;
+        }
+    } else if (depth > 2) {
+        // end early if past function layer
+        return;
+    }
+    // recursively handle next layer
+    depth++;
+    node.getChildren().forEach(c => parseAllChildren(c, depth));
+}
